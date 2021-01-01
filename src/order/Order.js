3ae0fdc975837eb3c5c554d07d03195bb1a11be6
Zeeshan Haider Shaheen <p156064@nu.edu.pk>
2021-01-01 13:55:40 +0500

    list orders

--- src/order/Order.js
+++ src/order/Order.js
@@ -24,13 +24,30 @@ const OrderContent = styled(DialogContent)`
   height: 100%;
 `;
 
+const OrderContainer = styled.div`
+  padding: 10px 0px;
+  border-bottom: 1px solid grey;
+`;
+
+const OrderItem = styled.div`
+  padding: 10px 0px;
+`;
+
 export function Order({ orders }) {
   return (
     <OrderStyled>
       {orders.length === 0 ? (
         <OrderContent>Your Order is Empty!</OrderContent>
       ) : (
-        <OrderContent>Found {orders.length} orders</OrderContent>
+        <OrderContent>
+          {" "}
+          <OrderContainer>Your Order: </OrderContainer>{" "}
+          {orders.map((order) => (
+            <OrderContainer>
+              <OrderItem>{order.name}</OrderItem>
+            </OrderContainer>
+          ))}
+        </OrderContent>
       )}
 
       <DialogFooter>
